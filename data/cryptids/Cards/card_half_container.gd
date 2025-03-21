4fd17ff73512a8849daeb2a6ee2814d333f6ec24
Tanner-Maxwell <115847594+Tanner-Maxwell@users.noreply.github.com>
2025-03-20 20:37:09 -0700

    fixed duh hand
    fixed duh hand

--- data/cryptids/Cards/card_half_container.gd
+++ data/cryptids/Cards/card_half_container.gd
@@ -4,10 +4,12 @@ extends VBoxContainer
 @export var highlighted = false
 @onready var top_half_container = %TopHalfContainer
 @onready var bottom_half_container = %BottomHalfContainer
-@onready var hand = $"../../.."
+@onready var hand
 @onready var card_dialog = $"../.."
 @onready var disabled = false
 
+func _ready():
+	hand = get_node("%Hand")
 
 ### Signal handler for input events
 #func _on_HBoxContainer_input_event(event):
